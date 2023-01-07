b324c9366541f80b50f1ad8702f8d3b1bf307bd7
Ganeshlobejjhj <118112179+Ganeshlobejjhj@users.noreply.github.com>
2023-01-07 16:41:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "BOT_token")
 DROPLINK_API = os.environ.get("DROPLINK_API")
 MDISK_API = os.environ.get("MDISK_API", "RX2H62ggETMf03f3UlIB")
 ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split("5869058469")) if os.environ.get("ADMINS") else []
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Ganeshmoury7797")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "GaneshMovies")
 DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Ganeshmoury7797:97541152@cluster0.kodqjqk.mongodb.net/?retryWrites=true&w=majority")
 WEBSITE = os.environ.get('WEBSITE')
 
